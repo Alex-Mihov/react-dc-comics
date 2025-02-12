1dad05275cbb65f53d4500d5d4f5f7afbc622298
Alex Mihov <alexmihov.as@gmail.com>
2025-02-12 16:38:21 +0100

    [feat] header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,49 @@
 export default function Footer() {
-    return (<footer>Footer</footer>)
+    return (
+        <footer>
+            <div className="footer-links">
+                <div>
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>DC</h3>
+                    <ul>
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>SITES</h3>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,23 @@ export default function Header() {
 
         <header>
             <div className="header-logo">
-
-
-                <img src="/img/dc-logo.png" alt="" />
-
-
+                <img src="/img/dc-logo.png" alt="Logo Dc" />
+            </div>
+            <div className="navigation">
+                <nav>
+                    <ul>
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </nav>
             </div>
         </header>
     )
