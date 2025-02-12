ec8d29933b57e3a85ae757ec1d6f0341f40ac4f9
Alex Mihov <alexmihov.as@gmail.com>
2025-02-12 15:40:28 +0100

    [feat] functions Herader Main Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// header 
+export default function Header() {
+    return (
+
+        <header>
+            <div className="header-logo">
+
+
+                <img src="/img/dc-logo.png" alt="" />
+
+
+            </div>
+        </header>
+    )
+}
