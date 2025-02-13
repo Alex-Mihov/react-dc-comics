ea8996faeb45a4bb165a847ba4dad30b99ef3e5b
Alex Mihov <alexmihov.as@gmail.com>
2025-02-13 16:18:53 +0100

    utilizzo di props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,22 @@ import Navbar from "./Navbar"
 
 // header 
 export default function Header() {
+
+    const navLinks = [
+        { id: 1, text: "CHARACTERS", url: "#" },
+        { id: 2, text: "COMICS", url: "#" },
+        { id: 3, text: "MOVIES", url: "#" },
+        { id: 4, text: "TV", url: "#" },
+        { id: 5, text: "GAMES", url: "#" },
+        { id: 6, text: "COLLECTIBLES", url: "#" },
+        { id: 7, text: "VIDEOS", url: "#" },
+        { id: 8, text: "FANS", url: "#" },
+        { id: 9, text: "NEWS", url: "#" },
+        { id: 10, text: "SHOP", url: "#" }
+    ];
+
     return (
+
 
         <header>
             <div className="container-header">
@@ -13,9 +28,12 @@ export default function Header() {
                     <Logo />
                 </div>
                 <div>
-                    <Navbar />
+                    <nav>
+                        <Navbar links={navLinks} />
+                    </nav>
+
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,18 @@
-const navLinks = [
-    { id: 1, text: "CHARACTERS", url: "#" },
-    { id: 2, text: "COMICS", url: "#" },
-    { id: 3, text: "MOVIES", url: "#" },
-    { id: 4, text: "TV", url: "#" },
-    { id: 5, text: "GAMES", url: "#" },
-    { id: 6, text: "COLLECTIBLES", url: "#" },
-    { id: 7, text: "VIDEOS", url: "#" },
-    { id: 8, text: "FANS", url: "#" },
-    { id: 9, text: "NEWS", url: "#" },
-    { id: 10, text: "SHOP", url: "#" }
-];
+
 
 export default function Navbar(props) {
 
 
     return (
-        <nav>
-            <ul>
-                {navLinks.map((link) => (
-                    <li key={link.id}>
-                        <a href={link.url}>{link.text}</a>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+
+        <ul>
+            {props.links.map((link) => (
+                <li key={link.id}>
+                    <a href={link.url}>{link.text}</a>
+                </li>
+            ))}
+        </ul>
+
     );
 
 }
